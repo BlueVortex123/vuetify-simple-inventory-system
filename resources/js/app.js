d0f51cf43540daa05a87d7bf16f143bad6cfd688
BlueVortex123 <duicu.andrei@gmail.com>
2023-11-16 01:05:10 +0200

    add LAravel Breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
+import './bootstrap';
+import '../css/app.css';
+
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { createVuetify } from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -11,14 +15,19 @@ const vuetify = createVuetify({
   components,
   directives,
 });
-
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .use(vuetify)
-      .mount(el)
-  },
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(vuetify)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
 });
